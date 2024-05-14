5022d5a2cab2b13820bfd79822484fd28fde542b
javier armero lopez <jaarlo@floridauniversitaria.es>
2024-05-14 16:39:28 +0200

    Agrego clase usuario y la bddatos

---
+++ src/registro mockup/BDatos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace registro_mockup
+{
+    internal class BDatos
+    {
+
+        private MySqlConnection conexion;
+
+        public MySqlConnection Conexion { get { return conexion; } }
+
+        public BDatos() { 
+                // Conexión Local
+                string server = "server=sql11.freesqldatabase.com;";
+                string port = "port=3306;";
+                string database = "database=sql11702602;";
+                string usuario = "uid=sql11702602;";
+                string password = "pwd=kyY7QPe339;";
+                string convert = "Convert Zero Datetime=true;";
+                string connectionstring = server + port + database + usuario + password + convert;
+
+                conexion = new MySqlConnection(connectionstring);
+            }
+
+            public bool AbrirConexion()
+            {
+                try
+                {
+                    conexion.Open();
+                    return true;
+                }
+                catch (MySqlException)
+                {
+                    return false;
+                }
+            }
+
+            public bool CerrarConexion()
+            {
+                try
+                {
+                    conexion.Close();
+                    return true;
+                }
+                catch (MySqlException)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+
