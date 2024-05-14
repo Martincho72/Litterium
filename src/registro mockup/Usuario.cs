d110fc73dad2a138a980f08167bcac9b9a49f5b9
javier armero lopez <jaarlo@floridauniversitaria.es>
2024-05-14 17:05:24 +0200

    funciones para agregar usuario a la bd

--- src/registro mockup/Usuario.cs
+++ src/registro mockup/Usuario.cs
@@ -22,6 +22,20 @@ namespace registro_mockup
 
 
 
+        public Usuario(string usuario,string clave,bool esAdmin,string nombre,string correoElectronico,string direccion,int telefono,
+            bool vetado,bool baja)
+        {
+            this.usuario = usuario;
+            this.clave = clave;
+            this.esAdmin = esAdmin;
+            this.nombre = nombre;
+            this.correoElectronico = correoElectronico;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.vetado = vetado;
+            this.baja = baja;
+        }
+
         public static bool EncontrarUsuario(MySqlConnection conexion, string usuario)
         {
             string consulta = string.Format("SELECT usuario FROM empleados WHERE usuario = '{0}'", usuario);
@@ -40,6 +54,28 @@ namespace registro_mockup
                 return false;
             }
         }
+        public int AgregarUsuario(MySqlConnection conexion, Usuario us1)
+        {
+            int vetado;
+            int baja;
+            int adminsitrador;
+            if (us1.esAdmin) adminsitrador = 1;
+            else adminsitrador = 0;
+            if (us1.baja) baja = 1;
+            else baja = 0;
+            if (us1.vetado) vetado = 1;
+            else vetado = 0;
 
+            int retorno;
+            string consulta = String.Format("INSERT INTO usuarios (usuario,contrasenya,esAdmin,nombre,correo,direccion,telefono,vetado,baja) " +
+                "VALUES " + "('{0}','{1}','{2}','{3}','{4}''{5}','{6}','{7}','{8}','{9}')", us1.usuario, us1.clave, adminsitrador, us1.nombre,
+                us1.correoElectronico, us1.direccion, us1.telefono,vetado,baja);
+
+            MySqlCommand comando = new MySqlCommand(consulta, conexion);
+
+            retorno = comando.ExecuteNonQuery();
+
+            return retorno;
+        }
     }
 }
