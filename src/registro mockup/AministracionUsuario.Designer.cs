d4a6f9e91919d740b2277bea93b9de7f0fe59a24
Martincho72 <aomartinbarrachina@gmail.com>
2024-05-15 22:24:06 +0200

    datasource, javichu haz las propiedades xd

--- src/registro mockup/AministracionUsuario.Designer.cs
+++ src/registro mockup/AministracionUsuario.Designer.cs
@@ -29,20 +29,10 @@
         private void InitializeComponent()
         {
             this.tlpBotones = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
             this.iconButton2 = new FontAwesome.Sharp.IconButton();
             this.iconButton1 = new FontAwesome.Sharp.IconButton();
             this.iconButton7 = new FontAwesome.Sharp.IconButton();
-            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
-            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.clave = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.admin = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.correo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.vetado = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.baja = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.tlpBotones.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
             this.SuspendLayout();
@@ -67,6 +57,18 @@
             this.tlpBotones.Size = new System.Drawing.Size(600, 81);
             this.tlpBotones.TabIndex = 0;
             // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.AllowUserToAddRows = false;
+            this.dgvUsuarios.AllowUserToDeleteRows = false;
+            this.dgvUsuarios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Location = new System.Drawing.Point(0, 141);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.ReadOnly = true;
+            this.dgvUsuarios.Size = new System.Drawing.Size(600, 150);
+            this.dgvUsuarios.TabIndex = 1;
+            // 
             // iconButton2
             // 
             this.iconButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
@@ -80,7 +82,7 @@
             this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.iconButton2.Location = new System.Drawing.Point(400, 2);
-            this.iconButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.iconButton2.Margin = new System.Windows.Forms.Padding(2);
             this.iconButton2.Name = "iconButton2";
             this.iconButton2.Padding = new System.Windows.Forms.Padding(7, 0, 14, 0);
             this.iconButton2.Size = new System.Drawing.Size(198, 77);
@@ -104,7 +106,7 @@
             this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.iconButton1.Location = new System.Drawing.Point(201, 2);
-            this.iconButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.iconButton1.Margin = new System.Windows.Forms.Padding(2);
             this.iconButton1.Name = "iconButton1";
             this.iconButton1.Padding = new System.Windows.Forms.Padding(7, 0, 14, 0);
             this.iconButton1.Size = new System.Drawing.Size(195, 77);
@@ -126,7 +128,7 @@
             this.iconButton7.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.iconButton7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.iconButton7.Location = new System.Drawing.Point(2, 2);
-            this.iconButton7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.iconButton7.Margin = new System.Windows.Forms.Padding(2);
             this.iconButton7.Name = "iconButton7";
             this.iconButton7.Padding = new System.Windows.Forms.Padding(7, 0, 14, 0);
             this.iconButton7.Size = new System.Drawing.Size(195, 77);
@@ -135,95 +137,6 @@
             this.iconButton7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.iconButton7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.iconButton7.UseVisualStyleBackColor = false;
-            // 
-            // dgvUsuarios
-            // 
-            this.dgvUsuarios.AllowUserToAddRows = false;
-            this.dgvUsuarios.AllowUserToDeleteRows = false;
-            this.dgvUsuarios.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.id,
-            this.usuario,
-            this.clave,
-            this.admin,
-            this.nombre,
-            this.correo,
-            this.direccion,
-            this.telefono,
-            this.vetado,
-            this.baja});
-            this.dgvUsuarios.Location = new System.Drawing.Point(0, 141);
-            this.dgvUsuarios.Name = "dgvUsuarios";
-            this.dgvUsuarios.ReadOnly = true;
-            this.dgvUsuarios.Size = new System.Drawing.Size(600, 150);
-            this.dgvUsuarios.TabIndex = 1;
-            // 
-            // id
-            // 
-            this.id.HeaderText = "id";
-            this.id.Name = "id";
-            this.id.ReadOnly = true;
-            // 
-            // usuario
-            // 
-            this.usuario.HeaderText = "usuario";
-            this.usuario.Name = "usuario";
-            this.usuario.ReadOnly = true;
-            // 
-            // clave
-            // 
-            this.clave.HeaderText = "clave";
-            this.clave.Name = "clave";
-            this.clave.ReadOnly = true;
-            // 
-            // admin
-            // 
-            this.admin.HeaderText = "admin";
-            this.admin.Name = "admin";
-            this.admin.ReadOnly = true;
-            this.admin.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.admin.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            // 
-            // nombre
-            // 
-            this.nombre.HeaderText = "nombre";
-            this.nombre.Name = "nombre";
-            this.nombre.ReadOnly = true;
-            // 
-            // correo
-            // 
-            this.correo.HeaderText = "correo";
-            this.correo.Name = "correo";
-            this.correo.ReadOnly = true;
-            // 
-            // direccion
-            // 
-            this.direccion.HeaderText = "direccion";
-            this.direccion.Name = "direccion";
-            this.direccion.ReadOnly = true;
-            // 
-            // telefono
-            // 
-            this.telefono.HeaderText = "telefono";
-            this.telefono.Name = "telefono";
-            this.telefono.ReadOnly = true;
-            // 
-            // vetado
-            // 
-            this.vetado.HeaderText = "vetado";
-            this.vetado.Name = "vetado";
-            this.vetado.ReadOnly = true;
-            this.vetado.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.vetado.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            // 
-            // baja
-            // 
-            this.baja.HeaderText = "baja";
-            this.baja.Name = "baja";
-            this.baja.ReadOnly = true;
-            this.baja.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.baja.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             // 
             // AministracionUsuario
             // 
@@ -249,15 +162,5 @@
         private FontAwesome.Sharp.IconButton iconButton1;
         private FontAwesome.Sharp.IconButton iconButton7;
         private System.Windows.Forms.DataGridView dgvUsuarios;
-        private System.Windows.Forms.DataGridViewTextBoxColumn id;
-        private System.Windows.Forms.DataGridViewTextBoxColumn usuario;
-        private System.Windows.Forms.DataGridViewTextBoxColumn clave;
-        private System.Windows.Forms.DataGridViewCheckBoxColumn admin;
-        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn correo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn direccion;
-        private System.Windows.Forms.DataGridViewTextBoxColumn telefono;
-        private System.Windows.Forms.DataGridViewCheckBoxColumn vetado;
-        private System.Windows.Forms.DataGridViewCheckBoxColumn baja;
     }
 }
--- src/registro mockup/AministracionUsuario.cs
+++ src/registro mockup/AministracionUsuario.cs
@@ -22,26 +22,7 @@ namespace registro_mockup
         {
             if (bDatos.AbrirConexion())
             {
-                List<Usuario> usuarios = Usuario.BuscarUsuarios(bDatos.Conexion);
-
-                // Iteramos sobre la lista y agregamos cada usuario al DataGridView
-                foreach (Usuario usuario in usuarios)
-                {
-                    // Agregamos una nueva fila al DataGridView
-                    int rowIndex = dgvUsuarios.Rows.Add();
-
-                    // Asignamos los valores del usuario a las celdas de la fila
-                    dgvUsuarios.Rows[rowIndex].Cells["ID"].Value = usuario.Id;
-                    dgvUsuarios.Rows[rowIndex].Cells["Usuario"].Value = usuario.Usu;
-                    dgvUsuarios.Rows[rowIndex].Cells["Clave"].Value = usuario.Clave;
-                    dgvUsuarios.Rows[rowIndex].Cells["admin"].Value = usuario.EsAdmin;
-                    dgvUsuarios.Rows[rowIndex].Cells["Nombre"].Value = usuario.Nombre;
-                    dgvUsuarios.Rows[rowIndex].Cells["Correo"].Value = usuario.Correo;
-                    dgvUsuarios.Rows[rowIndex].Cells["Direccion"].Value = usuario.Direccion;
-                    dgvUsuarios.Rows[rowIndex].Cells["Telefono"].Value = usuario.Telefono;
-                    dgvUsuarios.Rows[rowIndex].Cells["Vetado"].Value = usuario.Vetado;
-                    dgvUsuarios.Rows[rowIndex].Cells["Baja"].Value = usuario.Baja;
-                }
+                dgvUsuarios.DataSource = Usuario.BuscarUsuarios(bDatos.Conexion);
             }
             else { }
             bDatos.CerrarConexion();
