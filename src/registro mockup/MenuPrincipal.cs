0159604213e1399523ea558b12c067536297f8fc
i7_mg\Adrian <abc>
2024-05-15 19:26:13 +0200

    añadir dtgv

--- src/registro mockup/MenuPrincipal.cs
+++ src/registro mockup/MenuPrincipal.cs
@@ -26,6 +26,7 @@ namespace registro_mockup
             panelMenu.Controls.Add(bordeizqBTN);
             this.Text = string.Empty;
             this.ControlBox = false;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
             this.DoubleBuffered = true;
         }
 
