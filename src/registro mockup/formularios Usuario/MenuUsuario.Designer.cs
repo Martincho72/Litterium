85fe48c7352578b32f779b12694475f3f38bc15f
i7_mg\Adrian <abc>
2024-05-17 20:06:04 +0200

    hola

--- src/registro mockup/formularios Usuario/MenuUsuario.Designer.cs
+++ src/registro mockup/formularios Usuario/MenuUsuario.Designer.cs
@@ -28,12 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelUsuarios = new System.Windows.Forms.Panel();
+            this.tlpBotones = new System.Windows.Forms.TableLayoutPanel();
+            this.btnMisCortohistorias = new FontAwesome.Sharp.IconButton();
+            this.btnMiCuenta = new FontAwesome.Sharp.IconButton();
+            this.btnMisLibros = new FontAwesome.Sharp.IconButton();
+            this.btnMiHistorial = new FontAwesome.Sharp.IconButton();
+            this.btnMisBorradores = new FontAwesome.Sharp.IconButton();
+            this.panelUsuarios.SuspendLayout();
+            this.tlpBotones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelUsuarios
+            // 
+            this.panelUsuarios.Controls.Add(this.tlpBotones);
+            this.panelUsuarios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelUsuarios.Location = new System.Drawing.Point(0, 0);
+            this.panelUsuarios.Name = "panelUsuarios";
+            this.panelUsuarios.Size = new System.Drawing.Size(978, 639);
+            this.panelUsuarios.TabIndex = 8;
+            // 
+            // tlpBotones
+            // 
+            this.tlpBotones.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.tlpBotones.ColumnCount = 5;
+            this.tlpBotones.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpBotones.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpBotones.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpBotones.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpBotones.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpBotones.Controls.Add(this.btnMisBorradores, 0, 0);
+            this.tlpBotones.Controls.Add(this.btnMiHistorial, 0, 0);
+            this.tlpBotones.Controls.Add(this.btnMisCortohistorias, 0, 0);
+            this.tlpBotones.Controls.Add(this.btnMiCuenta, 0, 0);
+            this.tlpBotones.Controls.Add(this.btnMisLibros, 0, 0);
+            this.tlpBotones.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tlpBotones.Location = new System.Drawing.Point(0, 0);
+            this.tlpBotones.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tlpBotones.Name = "tlpBotones";
+            this.tlpBotones.RowCount = 1;
+            this.tlpBotones.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpBotones.Size = new System.Drawing.Size(978, 125);
+            this.tlpBotones.TabIndex = 5;
+            // 
+            // btnMisCortohistorias
+            // 
+            this.btnMisCortohistorias.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnMisCortohistorias.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnMisCortohistorias.FlatAppearance.BorderSize = 0;
+            this.btnMisCortohistorias.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMisCortohistorias.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMisCortohistorias.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMisCortohistorias.IconChar = FontAwesome.Sharp.IconChar.FileEdit;
+            this.btnMisCortohistorias.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMisCortohistorias.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMisCortohistorias.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMisCortohistorias.Location = new System.Drawing.Point(393, 2);
+            this.btnMisCortohistorias.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMisCortohistorias.Name = "btnMisCortohistorias";
+            this.btnMisCortohistorias.Padding = new System.Windows.Forms.Padding(10, 0, 21, 0);
+            this.btnMisCortohistorias.Size = new System.Drawing.Size(189, 121);
+            this.btnMisCortohistorias.TabIndex = 6;
+            this.btnMisCortohistorias.Text = "Mis Cortohistorias";
+            this.btnMisCortohistorias.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMisCortohistorias.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMisCortohistorias.UseVisualStyleBackColor = false;
+            this.btnMisCortohistorias.Click += new System.EventHandler(this.btnMisCortohistorias_Click);
+            // 
+            // btnMiCuenta
+            // 
+            this.btnMiCuenta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnMiCuenta.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnMiCuenta.FlatAppearance.BorderSize = 0;
+            this.btnMiCuenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMiCuenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMiCuenta.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMiCuenta.IconChar = FontAwesome.Sharp.IconChar.FileExcel;
+            this.btnMiCuenta.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMiCuenta.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMiCuenta.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMiCuenta.Location = new System.Drawing.Point(3, 2);
+            this.btnMiCuenta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMiCuenta.Name = "btnMiCuenta";
+            this.btnMiCuenta.Padding = new System.Windows.Forms.Padding(10, 0, 21, 0);
+            this.btnMiCuenta.Size = new System.Drawing.Size(189, 121);
+            this.btnMiCuenta.TabIndex = 5;
+            this.btnMiCuenta.Text = "Mi Cuenta";
+            this.btnMiCuenta.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMiCuenta.UseVisualStyleBackColor = false;
+            this.btnMiCuenta.Click += new System.EventHandler(this.btnMiCuenta_Click);
+            // 
+            // btnMisLibros
+            // 
+            this.btnMisLibros.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnMisLibros.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnMisLibros.FlatAppearance.BorderSize = 0;
+            this.btnMisLibros.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMisLibros.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMisLibros.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMisLibros.IconChar = FontAwesome.Sharp.IconChar.FileMedical;
+            this.btnMisLibros.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMisLibros.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMisLibros.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMisLibros.Location = new System.Drawing.Point(198, 2);
+            this.btnMisLibros.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMisLibros.Name = "btnMisLibros";
+            this.btnMisLibros.Padding = new System.Windows.Forms.Padding(10, 0, 21, 0);
+            this.btnMisLibros.Size = new System.Drawing.Size(189, 121);
+            this.btnMisLibros.TabIndex = 4;
+            this.btnMisLibros.Text = "Mis Libros";
+            this.btnMisLibros.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMisLibros.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMisLibros.UseVisualStyleBackColor = false;
+            this.btnMisLibros.Click += new System.EventHandler(this.btnMisLibros_Click);
+            // 
+            // btnMiHistorial
+            // 
+            this.btnMiHistorial.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnMiHistorial.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnMiHistorial.FlatAppearance.BorderSize = 0;
+            this.btnMiHistorial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMiHistorial.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMiHistorial.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMiHistorial.IconChar = FontAwesome.Sharp.IconChar.FileEdit;
+            this.btnMiHistorial.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMiHistorial.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMiHistorial.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMiHistorial.Location = new System.Drawing.Point(783, 2);
+            this.btnMiHistorial.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMiHistorial.Name = "btnMiHistorial";
+            this.btnMiHistorial.Padding = new System.Windows.Forms.Padding(10, 0, 21, 0);
+            this.btnMiHistorial.Size = new System.Drawing.Size(192, 121);
+            this.btnMiHistorial.TabIndex = 7;
+            this.btnMiHistorial.Text = "Mi Historial de compras";
+            this.btnMiHistorial.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMiHistorial.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMiHistorial.UseVisualStyleBackColor = false;
+            this.btnMiHistorial.Click += new System.EventHandler(this.btnMiHistorial_Click);
+            // 
+            // btnMisBorradores
+            // 
+            this.btnMisBorradores.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnMisBorradores.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnMisBorradores.FlatAppearance.BorderSize = 0;
+            this.btnMisBorradores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMisBorradores.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMisBorradores.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMisBorradores.IconChar = FontAwesome.Sharp.IconChar.FileEdit;
+            this.btnMisBorradores.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnMisBorradores.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnMisBorradores.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMisBorradores.Location = new System.Drawing.Point(588, 2);
+            this.btnMisBorradores.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMisBorradores.Name = "btnMisBorradores";
+            this.btnMisBorradores.Padding = new System.Windows.Forms.Padding(10, 0, 21, 0);
+            this.btnMisBorradores.Size = new System.Drawing.Size(189, 121);
+            this.btnMisBorradores.TabIndex = 8;
+            this.btnMisBorradores.Text = "Mis Borradores";
+            this.btnMisBorradores.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMisBorradores.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMisBorradores.UseVisualStyleBackColor = false;
+            this.btnMisBorradores.Click += new System.EventHandler(this.btnMisBorradores_Click);
+            // 
+            // MenuUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(978, 639);
+            this.Controls.Add(this.panelUsuarios);
+            this.Name = "MenuUsuario";
             this.Text = "MenuUsuario";
+            this.Load += new System.EventHandler(this.MenuUsuario_Load);
+            this.panelUsuarios.ResumeLayout(false);
+            this.tlpBotones.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel panelUsuarios;
+        private System.Windows.Forms.TableLayoutPanel tlpBotones;
+        private FontAwesome.Sharp.IconButton btnMisBorradores;
+        private FontAwesome.Sharp.IconButton btnMiHistorial;
+        private FontAwesome.Sharp.IconButton btnMisCortohistorias;
+        private FontAwesome.Sharp.IconButton btnMiCuenta;
+        private FontAwesome.Sharp.IconButton btnMisLibros;
     }
 }
--- src/registro mockup/formularios Usuario/MenuUsuario.cs
+++ src/registro mockup/formularios Usuario/MenuUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,57 @@ namespace registro_mockup.formularios_Usuario
 {
     public partial class MenuUsuario : Form
     {
+        private IconButton actualBTN;
+        private Panel bordeizqBTN;
+        private Form currentForm;
         public MenuUsuario()
         {
             InitializeComponent();
+        }
+
+        private void MenuUsuario_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void OpenChildForm(Form childForm)
+        {
+            if (currentForm != null)
+            {
+                currentForm.Close();
+            }
+            currentForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelUsuarios.Controls.Add(childForm);
+            panelUsuarios.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void btnMiCuenta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMisLibros_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMisCortohistorias_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMisBorradores_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMiHistorial_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
