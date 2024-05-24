15686de54d23e1fc0fe60ea44dfe97df1d9c4096
gacapa <gacapa@floridauniversitaria.es>
2024-05-24 17:14:23 +0200

    Titulo de Visualizar

--- src/registro mockup/Principal/VisualizarCortohistoria.Designer.cs
+++ src/registro mockup/Principal/VisualizarCortohistoria.Designer.cs
@@ -146,7 +146,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "VisualizarCortohistoria";
             this.Load += new System.EventHandler(this.VisualizarCortohistoria_Load);
             this.panel1.ResumeLayout(false);
